15deda7109475ab5e15042922152b55e32c6c785
Gab18 <softarthur18@gmail.com>
2022-05-25 12:26:09 -0400

    push all

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -3,11 +3,16 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Vendedor;
 
 class VendedorController {
     public static function crear( Router $router) {
 
-        $router->render('')
+        $errores = Vendedor::getErrores();
+
+        $router->render('vendedores/crear', [
+            'errores' => $errores
+        ]);
     }
 
     public static function actualizar() {
